6d5d3df460458384db956615ee88576d5d15aca9
jungeunji <ejjung312@gmail.com>
2025-04-20 17:40:04 +0900

    OCR 프로젝트 삭제

--- LicensePlateOCR/crnn_ocr.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class OCR {
-public:
-
-private:
-
-};
--- LicensePlateOCR/license_plate_ocr.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern "C" {
-	__declspec(dllexport) void InitOcrModel(const char* modelPath);
-
-	__declspec(dllexport) void RunOcr(unsigned char* data, char* ocrText);
-
-	__declspec(dllexport) void ReleaseOcrModel();
-}
